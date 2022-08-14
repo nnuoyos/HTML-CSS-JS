41df821bb2be8e88d398f4c8cc7f05b45125e52f
nnuoyos <youngadult3104@gmail.com>
2022-08-14 23:38:59 +0900

    클론코딩 마크업/페이지네이션 구현중

--- 07_Clone/04_더케이 호텔서울/main.js
+++ 07_Clone/04_더케이 호텔서울/main.js
@@ -8,3 +8,34 @@ toggleBtn.addEventListener('click', ()=>{
     menu.classList.toggle('active');
     language.classList.toggle('active');
 });
+/* slide */
+const slide = document.querySelector('.slide_box');
+let slideWidth = slide.clientWidth;
+
+const slideItemBox = document.querySelectorAll('slide_img_box');
+const maxSlide = slideItemBox.length;
+let slideNow = 1;
+
+/* pagination */
+const pagination = document.querySelector('.slide_pagination');
+
+
+for (let i = 0; i < maxSlide; i++) {
+    if (i === 0) pagination.innerHTML += `<li class="active">•</li>`;
+    else pagination.innerHTML += `<li>•</li>`;
+  }
+
+const paginationImg = document.querySelectorAll('.slide_pagination > li');
+console.log(paginationImg)
+
+for(let i=0; i<maxSlide; i++){
+    paginationImg[i].addEventListener("click",()=>{
+        slideNow = i+1;
+        const offset = slideWidth * (slideNow -1);
+        slideItemBox.forEach((i)=>{
+            i.setAttribute("style", `left: ${-offset}px`);
+        });
+        paginationImg.forEach((i)=>i.classList.remove("active"));
+        paginationImg[slideNow-1].classList.add("active");
+    });
+}
