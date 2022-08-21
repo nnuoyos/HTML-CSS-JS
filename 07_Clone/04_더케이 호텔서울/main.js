9e1ea7dba117fbf30cdf7c955ac99aaba8e17bb5
nnuoyos <youngadult3104@gmail.com>
2022-08-21 23:38:11 +0900

    메인슬라이드,페이지네이션 구현완료,main05마크업중

--- 07_Clone/04_더케이 호텔서울/main.js
+++ 07_Clone/04_더케이 호텔서울/main.js
@@ -1,58 +1,85 @@
+/* header 상단 toggle button */
+//변수 지정
 const toggleBtn = document.querySelector('.navbar_toggle_button');
 const menu = document.querySelector('.navbar_menu');
 const language = document.querySelector('.navbar_language');
 const menuBox = document.querySelector('.menu_box');
-/* header 상단 toggle button */
-toggleBtn.addEventListener('click', () => {
+//토글 버튼 클릭시 일어날 함수
+toggleBtn.addEventListener('click', (event) => {
     menuBox.classList.toggle('active');
     menu.classList.toggle('active');
     language.classList.toggle('active');
     event.currentTarget.classList.toggle('active-1');
 });
 
+/* main slide*/
+//변수 지정
+let sliderWrapper = document.querySelector('.container'); //최상위 요소 div 
+let sliderContainer = document.querySelector('#slide'); //ul
+let slides = document.querySelectorAll('.main_slide'); //li
+let totalSlides = slides.length; //슬라이드 (li) 개수
+let slideIndex = 0;
+let sliderWidth = sliderWrapper.clientWidth; //container의 width
+sliderContainer.style.width = sliderWidth*totalSlides+'px';
+showSlides() //페이지 열자마자 움직이고 있게 함수 먼저 부르기
 
-/* slide */
+//자동 슬라이드 함수
+function showSlides(){
+    for(let i=0; i<totalSlides;i++){
+        sliderContainer.style.left = -(sliderWidth * slideIndex)+'px';
+    }
+    slideIndex++;
+    if(slideIndex === totalSlides){
+        slideIndex = 0; //현재페이지가 마지막이라면, 다시 첫화면으로 되돌리기
+    }
+    setTimeout(showSlides, 3500);
+}
+
+/* pagination promotion*/
 $(function(){
-    let container = $('.slide_box');
-    let pagination = $('.slide_pagination > li');
+    let container = $('#promotion_ul');
+    let pagination = $('#promotion_list > li');
+    let pageBtn = $('#promotion_list > li > a');
     let nowIndex = 0;
-
+    //페이징버튼 클릭 시 일어날 함수
     pagination.click(function(e){
         e.preventDefault(); //기본속성 막기
         nowIndex = pagination.index(this); //현재페이지 첫번째
+        pagination.eq(nowIndex).parent().addClass('active'); //클릭한 탭의 상위요소에 액티브 추가
+        pagination.eq(nowIndex).parent().siblings().removeClass('active'); 
+        pageBtn.eq(nowIndex).parent().addClass('active'); //페이징 버튼
+        pageBtn.eq(nowIndex).parent().siblings().removeClass('active');
+        container.animate({left:-310*nowIndex});
+    });
+});
+/* pagination package */
+$(function(){
+    let container = $('#package_ul');
+    let pagination = $('#package_list > li');
+    let pageBtn = $('#package_list > li > a');
+    let nowIndex = 0;
+    pagination.click(function(e){
+        e.preventDefault();
+        nowIndex = pagination.index(this);
         pagination.eq(nowIndex).parent().addClass('active');
         pagination.eq(nowIndex).parent().siblings().removeClass('active');
+        pageBtn.eq(nowIndex).parent().addClass('active');
+        pageBtn.eq(nowIndex).parent().siblings().removeClass('active');
         container.animate({left:-310*nowIndex});
     });
 });
 
-$(function(){
-    let container_02 = $('.slide_box_main03');
-    let pagination_02 = $('.pagination03 > li');
-    let nowIndex_02 = 0;
-
-    pagination_02.click(function(e){
-        e.preventDefault(); //기본속성 막기
-        nowIndex_02 = pagination_02.index(this); //현재페이지 첫번째
-        pagination_02.eq(nowIndex_02).parent().addClass('active');
-        pagination_02.eq(nowIndex_02).parent().siblings().removeClass('active');
-        container_02.animate({left:-310*nowIndex_02});
-    });
-});
-
-
 /* tab menu */
+//페이지 들어가자마자 첫번째 화면 보이도록 구현 다시 하기
 $(function () {
     let tabAnchor = $('.tabs_nav li a');
     let tabPanel = $('.tabs_panel');
-
     //링크를 클릭하면 할 일
     tabAnchor.click(function(e){
         e.preventDefault(); //링크의 기본 속성 막아놓기
         //내가 클릭한 탭에만 active 부여하기
         tabAnchor.removeClass('active'); //액티브 모두 제거
         $(this).addClass('active'); //클릭이벤트가 발생한 그 요소에만 액티브 추가
-
         tabPanel.hide();
         //tab 01 클릭하면 $('#tab01').show();
         /* 어트리뷰트 불러오기 */
