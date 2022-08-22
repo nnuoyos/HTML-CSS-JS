e5a2d2c39e30c47ba8ddb6351cb7e389bbf9bda3
nnuoyos <youngadult3104@gmail.com>
2022-08-22 20:59:40 +0900

    마크업 중

--- 07_Clone/04_더케이 호텔서울/main.js
+++ 07_Clone/04_더케이 호텔서울/main.js
@@ -14,58 +14,58 @@ toggleBtn.addEventListener('click', (event) => {
 
 /* main slide*/
 //변수 지정
-let sliderWrapper = document.querySelector('.container'); //최상위 요소 div 
+let sliderWrapper = document.querySelector('.container'); //최상위 요소 div
 let sliderContainer = document.querySelector('#slide'); //ul
 let slides = document.querySelectorAll('.main_slide'); //li
 let totalSlides = slides.length; //슬라이드 (li) 개수
 let slideIndex = 0;
 let sliderWidth = sliderWrapper.clientWidth; //container의 width
-sliderContainer.style.width = sliderWidth*totalSlides+'px';
-showSlides() //페이지 열자마자 움직이고 있게 함수 먼저 부르기
+sliderContainer.style.width = sliderWidth * totalSlides + 'px';
+showSlides(); //페이지 열자마자 움직이고 있게 함수 먼저 부르기
 
 //자동 슬라이드 함수
-function showSlides(){
-    for(let i=0; i<totalSlides;i++){
-        sliderContainer.style.left = -(sliderWidth * slideIndex)+'px';
+function showSlides() {
+    for (let i = 0; i < totalSlides; i++) {
+        sliderContainer.style.left = -(sliderWidth * slideIndex) + 'px';
     }
     slideIndex++;
-    if(slideIndex === totalSlides){
+    if (slideIndex === totalSlides) {
         slideIndex = 0; //현재페이지가 마지막이라면, 다시 첫화면으로 되돌리기
     }
     setTimeout(showSlides, 3500);
 }
 
 /* pagination promotion*/
-$(function(){
+$(function () {
     let container = $('#promotion_ul');
     let pagination = $('#promotion_list > li');
     let pageBtn = $('#promotion_list > li > a');
     let nowIndex = 0;
     //페이징버튼 클릭 시 일어날 함수
-    pagination.click(function(e){
+    pagination.click(function (e) {
         e.preventDefault(); //기본속성 막기
         nowIndex = pagination.index(this); //현재페이지 첫번째
         pagination.eq(nowIndex).parent().addClass('active'); //클릭한 탭의 상위요소에 액티브 추가
-        pagination.eq(nowIndex).parent().siblings().removeClass('active'); 
+        pagination.eq(nowIndex).parent().siblings().removeClass('active');
         pageBtn.eq(nowIndex).parent().addClass('active'); //페이징 버튼
         pageBtn.eq(nowIndex).parent().siblings().removeClass('active');
-        container.animate({left:-310*nowIndex});
+        container.animate({ left: -310 * nowIndex });
     });
 });
 /* pagination package */
-$(function(){
+$(function () {
     let container = $('#package_ul');
     let pagination = $('#package_list > li');
     let pageBtn = $('#package_list > li > a');
     let nowIndex = 0;
-    pagination.click(function(e){
+    pagination.click(function (e) {
         e.preventDefault();
         nowIndex = pagination.index(this);
         pagination.eq(nowIndex).parent().addClass('active');
         pagination.eq(nowIndex).parent().siblings().removeClass('active');
         pageBtn.eq(nowIndex).parent().addClass('active');
         pageBtn.eq(nowIndex).parent().siblings().removeClass('active');
-        container.animate({left:-310*nowIndex});
+        container.animate({ left: -310 * nowIndex });
     });
 });
 
@@ -74,20 +74,33 @@ $(function(){
 $(function () {
     let tabAnchor = $('.tabs_nav li a');
     let tabPanel = $('.tabs_panel');
+
     //링크를 클릭하면 할 일
-    tabAnchor.click(function(e){
+    tabAnchor.click(function (e) {
         e.preventDefault(); //링크의 기본 속성 막아놓기
         //내가 클릭한 탭에만 active 부여하기
         tabAnchor.removeClass('active'); //액티브 모두 제거
         $(this).addClass('active'); //클릭이벤트가 발생한 그 요소에만 액티브 추가
         tabPanel.hide();
-        //tab 01 클릭하면 $('#tab01').show();
         /* 어트리뷰트 불러오기 */
         //let c = A.attr('href'); c라는 변수에 a링크 가져오기
         let $target = $(this).attr('href');
-        //console.log($target); 클릭한 탭의 링크값이 콘솔에 찍힌다
+
         //유저가 보고싶은 해당 탭은 target에 저장되어 있다 show로 보여주기
         $($target).show();
     });
 });
 
+/* $(document).ready(function(){
+   
+    $('ul.tabs li').click(function(){
+      var tab_id = $(this).attr('data-tab');
+   
+      $('ul.tabs li').removeClass('current');
+      $('.tab-content').removeClass('current');
+   
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+   
+  }) */
